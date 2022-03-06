c54a6664dfd8fa5d060b1f77fa370bdbd6517805
Gary Rixon <gary.rixon@justpark.com>
2022-03-06 16:21:28 +0000

    return the content of an env var provided by the user

--- src/main.go
+++ src/main.go
@@ -12,6 +12,14 @@ func main() {
 		fmt.Fprintf(w, os.Getenv("HOSTNAME"))
 	})
 
+	http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
+		param := r.URL.Query().Get("envvar")
+		log.Print(param)
+		if param != "" {
+			fmt.Fprintf(w, os.Getenv(param))
+		}
+	})
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
